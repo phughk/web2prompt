3189da851865cc9856a492515161ee9ac0d6ed32
Hugh Kaznowski <phkaznowski@gmail.com>
2023-03-19 16:11:32 +0000

    Add CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+use clap::arg;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(short='t', long="time", default_value="now")]
+    time: String,
+    query: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Hello, world! {:?}", args);
 }
